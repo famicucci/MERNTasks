d50e80a4b0c372402a4d1a10a710e01cec17d279
Francisco Micucci <famicucci@gmail.com>
2021-04-06 19:58:14 -0300

    Poner el nombre de proyecto actual en Lista Tareas

--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,7 +1,18 @@
-import React, { Fragment } from 'react';
+import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
+import proyectoContext from '../../context/proyectos/proyectoContext';
 
 const ListadoTareass = () => {
+	// Obtener el State de proyectos
+	const proyectosContext = useContext(proyectoContext);
+	const { proyecto } = proyectosContext;
+
+	// Si no hay proyecto seleccionado
+	if (!proyecto) return <h2>Seleciona un proyecto</h2>;
+
+	// Destructuring para extraer el proyecto actual
+	const [proyectoActual] = proyecto;
+
 	const tareasProyecto = [
 		{ nombre: 'Elegir Plataforma', estado: true },
 		{ nombre: 'Elegir Colores', estado: false },
@@ -11,7 +22,7 @@ const ListadoTareass = () => {
 
 	return (
 		<Fragment>
-			<h2>Proyecto: Tienda Virtual</h2>
+			<h2>Proyecto: {proyectoActual.nombre}</h2>
 			<ul className="listado-tareas">
 				{tareasProyecto.length === 0 ? (
 					<li className="tarea">

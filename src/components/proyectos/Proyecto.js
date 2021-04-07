74439b58637c55703e47601fef81eca2b22e15e8
Francisco Micucci <famicucci@gmail.com>
2021-04-07 13:02:15 -0300

    Muestra las tareas de cada proyecto

--- src/components/proyectos/Proyecto.js
+++ src/components/proyectos/Proyecto.js
@@ -1,11 +1,21 @@
 import React, { useContext } from 'react';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const Proyecto = ({ proyecto }) => {
 	// Obtener el State de proyectos
 	const proyectosContext = useContext(proyectoContext);
-
 	const { proyectoActual } = proyectosContext;
+
+	// Obtener la funcion del context de tarea
+	const tareasContext = useContext(tareaContext);
+	const { obtenerTareas } = tareasContext;
+
+	// Función para agregar el proyecto actual
+	const seleccionarProyecto = (id) => {
+		proyectoActual(id); //Fijar un proyecto actual
+		obtenerTareas(id); //Filtrar las tareas cuando se de click
+	};
 
 	return (
 		<li>
@@ -13,7 +23,7 @@ const Proyecto = ({ proyecto }) => {
 				type="button"
 				className="btn btn-blank"
 				onClick={() => {
-					proyectoActual(proyecto.id);
+					seleccionarProyecto(proyecto.id);
 				}}
 			>
 				{proyecto.nombre}
--- src/components/tareas/ListadoTareas.js
+++ src/components/tareas/ListadoTareas.js
@@ -1,24 +1,22 @@
 import React, { Fragment, useContext } from 'react';
 import Tarea from './Tarea';
 import proyectoContext from '../../context/proyectos/proyectoContext';
+import tareaContext from '../../context/tareas/tareaContext';
 
 const ListadoTareass = () => {
 	// Obtener el State de proyectos
 	const proyectosContext = useContext(proyectoContext);
 	const { proyecto, eliminarProyecto } = proyectosContext;
 
+	// Obtener las tareas del proyecto
+	const tareasContext = useContext(tareaContext);
+	const { tareasproyecto } = tareasContext;
+
 	// Si no hay proyecto seleccionado
 	if (!proyecto) return <h2>Seleciona un proyecto</h2>;
 
 	// Destructuring para extraer el proyecto actual
 	const [proyectoActual] = proyecto;
-
-	const tareasProyecto = [
-		{ nombre: 'Elegir Plataforma', estado: true },
-		{ nombre: 'Elegir Colores', estado: false },
-		{ nombre: 'Elegir Plataformas de Pago', estado: false },
-		{ nombre: 'Elegir Hosting', estado: true },
-	];
 
 	// Elimina un proyecto
 	const onClickEliminar = () => {
@@ -29,12 +27,12 @@ const ListadoTareass = () => {
 		<Fragment>
 			<h2>Proyecto: {proyectoActual.nombre}</h2>
 			<ul className="listado-tareas">
-				{tareasProyecto.length === 0 ? (
+				{tareasproyecto.length === 0 ? (
 					<li className="tarea">
 						<p>No hay Tareas</p>
 					</li>
 				) : (
-					tareasProyecto.map((tarea) => <Tarea tarea={tarea} />)
+					tareasproyecto.map((tarea) => <Tarea tarea={tarea} />)
 				)}
 			</ul>
 

f9b24219363740825c3fadbb9788499d6d99a39f
Francisco Micucci <famicucci@gmail.com>
2021-04-13 23:13:39 -0300

    Permite Cerrar la Sesion

--- src/components/layout/Barra.js
+++ src/components/layout/Barra.js
@@ -1,14 +1,31 @@
-import React from 'react';
+import React, { useEffect, useContext } from 'react';
+import AuthContext from '../../context/autenticacion/authContext';
 
 const Barra = () => {
+	// Extraer la informacion de autenticacion
+	const authContext = useContext(AuthContext);
+	const { usuario, usuarioAutenticado, cerrarSesion } = authContext;
+
+	useEffect(() => {
+		usuarioAutenticado();
+	}, []);
+
 	return (
 		<header className="app-header">
-			<p className="nombre-usuario">
-				Hola <span>Francisco Micucci</span>
-			</p>
-
+			{usuario ? (
+				<p className="nombre-usuario">
+					Hola <span>{usuario.nombre}</span>
+				</p>
+			) : null}
 			<nav className="nav-principal">
-				<a href="#!">Cerrar Sesión</a>
+				<button
+					className="btn btn-blank cerrar-sesion"
+					onClick={() => {
+						cerrarSesion();
+					}}
+				>
+					Cerrar Sesión
+				</button>
 			</nav>
 		</header>
 	);
